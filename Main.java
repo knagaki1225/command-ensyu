83a5d7325e4a61fa2f1dffdeb2419808d534e74c
KazuyaNagaki <k.nagaki.sys23@morijyobi.ac.jp>
2023-11-09 10:14:15 +0900

    Main.javaの修正

--- Main.java
+++ Main.java
@@ -14,5 +14,17 @@ public class Main{
         double bmi = weight / height;
 
         System.out.println("BMI:" + String.format("%.2f", bmi));
+    	
+    	System.out.print("height(cm):");
+        height = Integer.parseInt(sc.nextLine());
+        System.out.print("weight(kg):");
+        weight = Integer.parseInt(sc.nextLine());
+
+        height /= 100;
+        height *= height;
+
+        bmi = weight / height;
+
+        System.out.println("BMI:" + String.format("%.2f", bmi));
     }
 }
